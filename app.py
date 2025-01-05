8824a5e46cb8f23345231a4c7acc0895b219a74c
Haridas Nishita <haridasnishita@gmail.com>
2025-01-05 20:46:08 +0530

    Web application is now predicting

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = '.')
 
 # Load the model
 regmodel = pickle.load(open('regmodel.pkl','rb'))
@@ -23,7 +23,16 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text = "The predicted House price is {}".format(output))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
-    
+
